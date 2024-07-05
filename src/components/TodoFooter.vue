38bf962c50955fa714161ab89059101b397258f0
NewBean0312 <newbean0312@gmail.com>
2024-07-05 13:39:19 +0900

    TodoFooter 컴포넌트 구현

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>footer</div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  margin: 0 auto;
+  border-radius: 5px;
+  background-color: #fff;
+  line-height: 50px;
+}
 
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
 </style>
